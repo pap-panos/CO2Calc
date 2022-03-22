a3b5bff2d738c6fa375d36ff646b5b3458ee1def
Giannis-Ttl <johntitil559@gmail.com>
2022-03-22 23:05:49 +0200

    Users Table

--- src/components/Administrative/Users.jsx
+++ src/components/Administrative/Users.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default Users
+  const getUsers = async () => {
+    await Axios.get("/api/users/").then(
+      (res) => {
+        if (res.data.length > 0) {
+          setUsers(res.data);
+        } else {
+          setUsers([]);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="container bg-white rounded my-2 p-3 d-flex justify-content-center">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">e-mail</th>
+            <th scope="col">Username</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            if (user.username !== "admin") {
+              return (
+                <tr key={user._id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{user.email}</td>
+                  <td>{user.username}</td>
+                  <td>Edit/Delete</td>
+                </tr>
+              );
+            } else {
+              return <tr key={user._id}></tr>;
+            }
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Users;
