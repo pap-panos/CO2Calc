08715d2317c3f3ff6aba97c4bcd7e24cd3d3230e
pap-panos <panospaps97@gmail.com>
2022-04-01 23:37:38 +0300

    tabIndex

--- src/components/Dashboard/ModalAnswers.jsx
+++ src/components/Dashboard/ModalAnswers.jsx
@@ -6,7 +6,7 @@ const ModalAnswers = () => {
       <div
         className="modal fade"
         id="exampleModal"
-        tabindex="-1"
+        tabIndex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
