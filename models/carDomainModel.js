829101bc8e6ccc71a422ea01fc7372a009deda10
pap-panos <panospaps97@gmail.com>
2022-08-05 13:07:20 +0300

    changes

--- models/carDomainModel.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const { carDomainSchema } = require("../schemas/carDomainSchema");
-
-const CarDomain = mongoose.model("carDomains", carDomainSchema);
-module.exports = { CarDomain };
--- models/countryModel.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const { countrySchema } = require("../schemas/countrySchema");
-
-const Country = mongoose.model("countries", countrySchema);
-module.exports = { Country };
